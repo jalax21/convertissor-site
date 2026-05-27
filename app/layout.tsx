c299ccff35cd9be5010eb84498b03fd1e63c1a69
jalax21 <148368064+jalax21@users.noreply.github.com>
2026-05-27 10:46:58 +0200

    Mise à jour du site

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -37,6 +38,9 @@ export default function RootLayout({
 
     <body className="min-h-full flex flex-col">
       {children}
+
+      <Analytics />
+      
     </body>
   </html>
 );
