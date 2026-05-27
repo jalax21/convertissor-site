8659fb61a2a3aae2d57df25552426f3a27bde1e3
jalax21 <148368064+jalax21@users.noreply.github.com>
2026-05-27 09:04:17 +0200

    Fix locale property

--- i18n.ts
+++ i18n.ts
@@ -1,8 +1,8 @@
 import {getRequestConfig} from "next-intl/server"
 
 export default getRequestConfig(async ({locale}) => ({
-  locale: locale || "fr",
+  locale: locale ?? "fr",
   messages: (
-    await import(`./messages/${locale || "fr"}.json`)
+    await import(`./messages/${locale ?? "fr"}.json`)
   ).default
 }))
