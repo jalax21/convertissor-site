b5a685e0d315831c2e2a573c0309250db3d5ea62
jalax21 <148368064+jalax21@users.noreply.github.com>
2026-05-30 17:55:59 +0200

    Mise à jour du site

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,19 +1,49 @@
 import { MetadataRoute } from "next"
 import { mathsFiches } from "@/data/revisions/maths"
+import { francaisFiches } from "@/data/revisions/francais"
+import { histoireFiches } from "@/data/revisions/histoire"
+import { svtFiches } from "@/data/revisions/svt"
+import { paysFiches } from "@/data/revisions/pays"
+import { geographieFiches } from "@/data/revisions/geographie"
 
 export default function sitemap(): MetadataRoute.Sitemap {
-
   const baseUrl = "https://quickunits.fr"
 
-  const mathsPages = mathsFiches.map((fiche) => ({
-    url: `${baseUrl}/fr/revisions/maths/${fiche.slug}`,
-    lastModified: new Date(),
-  }))
+  const pages = [
+    ...mathsFiches.map((fiche) => ({
+      url: `${baseUrl}/fr/revisions/maths/${fiche.slug}`,
+      lastModified: new Date(),
+    })),
+
+    ...francaisFiches.map((fiche) => ({
+      url: `${baseUrl}/fr/revisions/francais/${fiche.slug}`,
+      lastModified: new Date(),
+    })),
+
+    ...histoireFiches.map((fiche) => ({
+      url: `${baseUrl}/fr/revisions/histoire/${fiche.slug}`,
+      lastModified: new Date(),
+    })),
+
+    ...svtFiches.map((fiche) => ({
+      url: `${baseUrl}/fr/revisions/svt/${fiche.slug}`,
+      lastModified: new Date(),
+    })),
+
+    ...paysFiches.map((fiche) => ({
+      url: `${baseUrl}/fr/revisions/pays/${fiche.slug}`,
+      lastModified: new Date(),
+    })),
+
+    ...geographieFiches.map((fiche) => ({
+      url: `${baseUrl}/fr/revisions/geographie/${fiche.slug}`,
+      lastModified: new Date(),
+    })),
+  ]
 
   return [
-
     {
-      url: `${baseUrl}`,
+      url: baseUrl,
       lastModified: new Date(),
     },
 
@@ -32,7 +62,31 @@ export default function sitemap(): MetadataRoute.Sitemap {
       lastModified: new Date(),
     },
 
-    ...mathsPages,
+    {
+      url: `${baseUrl}/fr/revisions/francais`,
+      lastModified: new Date(),
+    },
 
+    {
+      url: `${baseUrl}/fr/revisions/histoire`,
+      lastModified: new Date(),
+    },
+
+    {
+      url: `${baseUrl}/fr/revisions/svt`,
+      lastModified: new Date(),
+    },
+
+    {
+      url: `${baseUrl}/fr/revisions/pays`,
+      lastModified: new Date(),
+    },
+
+    {
+      url: `${baseUrl}/fr/revisions/geographie`,
+      lastModified: new Date(),
+    },
+
+    ...pages,
   ]
 }
